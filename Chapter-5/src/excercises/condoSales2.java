5cb64ffe4cd1f3a4030398b0201f6cb58c0188cb
MHancock5466 <mhancock5466@gmail.com>
2017-02-21 14:09:27 -0600

    Add files via upload

---
+++ Chapter-5/src/excercises/condoSales2.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package excercises;
+
+/**
+ * @author MH137428
+ *
+ */
+import javax.swing.JOptionPane;
+public class condoSales2 
+{
+
+	public static void main(String[] args) 
+	{
+		String msg;
+		int condo, garage, parkCost = 150000, golfCost = 170000, lakeCost = 210000, garageCost = 5000;
+		
+		condo = Integer.parseInt(JOptionPane.showInputDialog(null, "1 = Park View\n2 = Golf Course View\n3 = Lake View"
+				+ "\nWhere would you like to stay?"));
+		garage = Integer.parseInt(JOptionPane.showInputDialog(null, "1 = Yes\n2 = No\nWould you like a garage?"));
+		
+		if(condo == 1)
+		{
+			if(garage == 1)
+			{
+				msg = "Your cost will be $" + (parkCost+garageCost);
+			}
+			if(garage == 2)
+			{
+				msg = "Your cost will be $" + parkCost;
+			}
+			else
+				msg = "Invalid Input";
+		}
+		if(condo == 2)
+			if(garage == 1)
+			{
+				msg = "Your cost will be $" + (golfCost+garageCost);
+			}
+			if(garage == 2)
+			{
+				msg = "Your cost will be $" + golfCost;
+			}
+			else
+				msg = "Invalid Input";
+		if(condo == 3)
+		{
+			if(garage == 1)
+			{
+				msg = "Your cost will be $" + (lakeCost+garageCost);
+			}
+			if(garage == 2)
+			{
+				msg = "Your cost will be $" + lakeCost;
+			}
+			else
+				msg = "Invalid Input";
+		}
+		JOptionPane.showMessageDialog(null, msg);
+	}
+
+}
